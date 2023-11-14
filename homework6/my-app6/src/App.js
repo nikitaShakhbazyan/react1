20f3f5dfce090ad5f3bbf1358021d8d4a6d0af81
nickshakh <shakhbazyann@gmail.com>
2023-11-14 19:03:09 +0400

    initial commit-2

---
+++ homework6/my-app6/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Fetching from './Fetching/Fetch';
+
+function App() {
+  return (
+    <div className="App">
+      <Fetching/>
+    </div>
+  );
+}
+
+export default App;
